9ca15d7c56e75c1047e4779c4b4443ac641347c3
yahya <yahya.banouk@uit.ac.ma>
2022-03-29 02:25:13 +0200

    screens

--- app/src/main/java/com/example/schoolmanagement/Login.java
+++ app/src/main/java/com/example/schoolmanagement/Login.java
@@ -2,13 +2,69 @@ package com.example.schoolmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    private EditText mailID , passwordID;
+    private FirebaseAuth mAuth;
+// ...
+// Initialize Firebase Auth
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginView);
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+
+            startActivity(new Intent(this,Login.class));
+
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Configure Google Sign In
+
+
+
+
+//        if(view.getId()==R.id.buttondlprof && visible == false)
+//        {
+//            zbbitxt.setVisibility(View.VISIBLE);
+//            visible= true;
+//
+//        }
+//        else if (view.getId()==R.id.buttondlprof && visible == true)
+//        {
+//            zbbitxt.setVisibility(View.INVISIBLE);
+//            visible= false;
+//
+//        }
+
     }
 }
---
+++ app/src/main/java/com/example/schoolmanagement/Register.java
@@ -0,0 +1,14 @@
+package com.example.schoolmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_screen);
+    }
+}
